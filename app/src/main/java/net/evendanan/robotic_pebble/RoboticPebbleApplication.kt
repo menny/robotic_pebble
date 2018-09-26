fceaecde4fc64c6bab2a2a0c056fcee132931462
Menny Even Danan <menny@evendanan.net>
2018-09-26 11:48:57 -0400

    Preparing release APK

--- app/src/main/java/net/evendanan/robotic_pebble/RoboticPebbleApplication.kt
+++ app/src/main/java/net/evendanan/robotic_pebble/RoboticPebbleApplication.kt
@@ -6,14 +6,12 @@ import android.os.StrictMode
 class RoboticPebbleApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            //this is required since Android VM will crash the app if we use file:// URI.
-            //But, since the Pebble app was built with support for file:// URIs....
-            //the other way to do it would be to use FileProvider, but the Pebble app is only
-            //allowing content:// from well-known providers (gmail, Inbox, downloads and a few others)
-            StrictMode.VmPolicy.Builder().apply {
-                StrictMode.setVmPolicy(build())
-            }
+        //this is required since Android VM will crash the app if we use file:// URI.
+        //But, since the Pebble app was built with support for file:// URIs....
+        //the other way to do it would be to use FileProvider, but the Pebble app is only
+        //allowing content:// from well-known providers (gmail, Inbox, downloads and a few others)
+        StrictMode.VmPolicy.Builder().apply {
+            StrictMode.setVmPolicy(build())
         }
     }
 }
